a3e220a9dbea41705055f5ad6149a0d4ab04e5f4
Shih-Min Lee <ssmlee04@gmail.com>
2020-12-06 17:26:39 -0500

    update google trends component

--- dist/index.js
+++ dist/index.js
@@ -113,6 +113,7 @@ function (_React$Component) {
         }),
         datasets: [{
           // label: 'interest over time',
+          yAxisID: '1',
           fill: false,
           lineTension: 0.1,
           backgroundColor: 'crimson',
@@ -124,7 +125,7 @@ function (_React$Component) {
           pointBorderColor: 'crimson',
           // pointBackgroundColor: '#fff',
           pointBorderWidth: 1,
-          pointHoverRadius: 5,
+          pointHoverRadius: 2,
           // pointHoverBackgroundColor: 'rgba(75,192,192,1)',
           pointHoverBorderColor: 'rgba(220,220,220,1)',
           pointHoverBorderWidth: 2,
@@ -149,6 +150,7 @@ function (_React$Component) {
             }
           }],
           yAxes: [{
+            id: '1',
             ticks: {
               fontSize: 8
             }
--- src/index.js
+++ src/index.js
@@ -49,6 +49,7 @@ export class GoogleTrends extends React.Component {
       datasets: [
         {
           // label: 'interest over time',
+          yAxisID: '1',
           fill: false,
           lineTension: 0.1,
           backgroundColor: 'crimson',
@@ -60,7 +61,7 @@ export class GoogleTrends extends React.Component {
           pointBorderColor: 'crimson',
           // pointBackgroundColor: '#fff',
           pointBorderWidth: 1,
-          pointHoverRadius: 5,
+          pointHoverRadius: 2,
           // pointHoverBackgroundColor: 'rgba(75,192,192,1)',
           pointHoverBorderColor: 'rgba(220,220,220,1)',
           pointHoverBorderWidth: 2,
@@ -84,6 +85,7 @@ export class GoogleTrends extends React.Component {
           },
         }],
         yAxes: [{
+          id: '1',
           ticks: {
             fontSize: 8
           },
