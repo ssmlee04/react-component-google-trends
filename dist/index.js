7ef167bd3cf62829171bed7d2773e104585fcf07
Shih-Min Lee <ssmlee04@gmail.com>
2020-12-06 17:29:33 -0500

    update google trends component

--- dist/index.js
+++ dist/index.js
@@ -112,25 +112,14 @@ function (_React$Component) {
           return d.formattedTime;
         }),
         datasets: [{
-          // label: 'interest over time',
-          yAxisID: '1',
+          type: 'line',
           fill: false,
-          lineTension: 0.1,
-          backgroundColor: 'crimson',
-          borderColor: 'crimson',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'crimson',
-          // pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
+          backgroundColor: 'darkred',
+          borderColor: 'darkred',
+          lineTension: 0,
+          borderWidth: 1,
+          pointRadius: 2,
           pointHoverRadius: 2,
-          // pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 2,
           data: initialData.map(function (d) {
             return d.value[0];
           })
@@ -150,7 +139,6 @@ function (_React$Component) {
             }
           }],
           yAxes: [{
-            id: '1',
             ticks: {
               fontSize: 8
             }
--- src/index.js
+++ src/index.js
@@ -48,25 +48,14 @@ export class GoogleTrends extends React.Component {
       labels: initialData.map(d => d.formattedTime),
       datasets: [
         {
-          // label: 'interest over time',
-          yAxisID: '1',
+          type: 'line',
           fill: false,
-          lineTension: 0.1,
-          backgroundColor: 'crimson',
-          borderColor: 'crimson',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'crimson',
-          // pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
+          backgroundColor: 'darkred',
+          borderColor: 'darkred',
+          lineTension: 0,
+          borderWidth: 1,
+          pointRadius: 2,
           pointHoverRadius: 2,
-          // pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 2,
           data: initialData.map(d => d.value[0])
         }
       ]
@@ -85,7 +74,6 @@ export class GoogleTrends extends React.Component {
           },
         }],
         yAxes: [{
-          id: '1',
           ticks: {
             fontSize: 8
           },
