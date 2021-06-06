fd26f2e2207184d8e4184eab64af144fc72c5f1f
Shih-Min Lee <ssmlee04@Shih-Mins-MacBook-Pro.local>
2021-06-06 14:24:23 -0400

    major changes to ui

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,8 @@ exports["default"] = exports.GoogleTrends = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _dayjs = _interopRequireDefault(require("dayjs"));
+
 var _lodash = _interopRequireDefault(require("lodash"));
 
 var _reactChartjs = require("react-chartjs-2");
@@ -49,15 +51,6 @@ function (_React$Component) {
   }
 
   _createClass(GoogleTrends, [{
-    key: "shouldComponentUpdate",
-    value: function shouldComponentUpdate(nextProps, nextState) {
-      var profile = this.props.profile;
-      if (!profile) return true;
-      if (nextState.copied) return true;
-      if (profile.ticker !== nextProps.profile.ticker) return true;
-      return false;
-    }
-  }, {
     key: "render",
     value: function render() {
       var _this2 = this;
@@ -67,19 +60,23 @@ function (_React$Component) {
           _this$props$prop = _this$props.prop,
           prop = _this$props$prop === void 0 ? 'google_trends' : _this$props$prop,
           _this$props$imgProp = _this$props.imgProp,
-          imgProp = _this$props$imgProp === void 0 ? 'google_trends_img' : _this$props$imgProp;
+          imgProp = _this$props$imgProp === void 0 ? 'google_trends_img' : _this$props$imgProp,
+          _this$props$theme = _this$props.theme,
+          theme = _this$props$theme === void 0 ? 'light' : _this$props$theme,
+          _this$props$count = _this$props.count,
+          count = _this$props$count === void 0 ? 60 : _this$props$count;
       var copied = this.state.copied;
 
       if (!profile) {
         return _react["default"].createElement("div", {
           style: {
-            fontSize: 8
+            fontSize: 12
           }
         }, "Not available at this time... ");
       }
 
       if (profile[imgProp] && profile[imgProp].url) {
-        var btnClass = copied ? 'react-components-show-url btn btn-sm btn-danger disabled font-8' : 'react-components-show-url btn btn-sm btn-warning font-8';
+        var btnClass = copied ? 'react-components-show-url btn btn-sm btn-danger disabled font-12' : 'react-components-show-url btn btn-sm btn-warning font-12';
         var btnText = copied ? 'Copied' : 'Copy Img';
         return _react["default"].createElement("div", {
           className: "react-components-show-button"
@@ -102,24 +99,23 @@ function (_React$Component) {
         }, btnText)));
       }
 
-      var keyword = profile.keyword || '';
+      var initialData = _lodash["default"].get(profile, "".concat(prop, ".data"), []).slice(-1 * count);
 
-      var initialData = _lodash["default"].get(profile, "".concat(prop, ".data"), []);
-
+      var keyword = _lodash["default"].get(profile, "".concat(prop, ".keyword")) || profile.keyword;
       var data = {
         labels: initialData.map(function (d) {
-          return d.formattedTime;
+          return _dayjs["default"].unix(d.time).format('YYYYMM');
         }),
         datasets: [{
-          fill: false,
+          fill: true,
           lineTension: 0.1,
-          backgroundColor: 'crimson',
-          borderColor: 'crimson',
+          backgroundColor: '#4285F4',
+          borderColor: '#4285F4',
           borderCapStyle: 'butt',
           // borderDash: [],
           // borderDashOffset: 0.0,
           // borderJoinStyle: 'miter',
-          pointBorderColor: 'crimson',
+          pointBorderColor: '#4285F4',
           // pointBackgroundColor: '#fff',
           borderWidth: 2,
           pointBorderWidth: 1,
@@ -130,26 +126,30 @@ function (_React$Component) {
           pointRadius: 0,
           // pointHitRadius: 2,
           data: initialData.map(function (d) {
-            return d.value[0];
+            return d.value;
           })
         }]
       };
+      var fontColor = theme === 'light' ? '#222222' : '#dddddd';
       var options = {
         legend: {
           display: false,
           label: {
-            fontSize: 8
+            fontColor: fontColor,
+            fontSize: 12
           }
         },
         scales: {
           xAxes: [{
             ticks: {
-              fontSize: 8
+              fontColor: fontColor,
+              fontSize: 12
             }
           }],
           yAxes: [{
             ticks: {
-              fontSize: 8
+              fontColor: fontColor,
+              fontSize: 12
             }
           }]
         }
@@ -158,34 +158,42 @@ function (_React$Component) {
         style: {
           width: '100%',
           padding: 5,
-          fontSize: 8
-        }
+          fontSize: 12
+        },
+        className: "theme-black-".concat(theme)
       }, _react["default"].createElement("div", {
+        className: "theme-darkred-".concat(theme),
         style: {
-          color: 'darkred',
           fontWeight: 'bold'
         }
-      }, _react["default"].createElement("span", {
-        className: "green"
-      }, "Keyword trends: "), keyword), _react["default"].createElement(_reactChartjs.Line, {
+      }, profile.ticker, " - ", profile.name, "\xA0", _react["default"].createElement("span", {
+        className: "theme-green-".concat(theme)
+      }, "Google 5-year Trends")), keyword ? _react["default"].createElement("span", {
+        style: {
+          fontSize: 12
+        }
+      }, "Keyword: ", _react["default"].createElement("b", {
+        className: "theme-red-".concat(theme)
+      }, keyword)) : null, _react["default"].createElement(_reactChartjs.Line, {
         data: data,
         height: 160,
         options: options
       }), _react["default"].createElement("div", {
         style: {
-          fontSize: 8,
-          color: 'gray'
+          fontSize: 12,
+          padding: 5,
+          paddingTop: 2
         }
-      }, "Generated by ", _react["default"].createElement("span", {
-        style: {
-          color: 'darkred'
-        }
+      }, "Generated by ", _react["default"].createElement("a", {
+        href: "https://twitter.com/earningsfly",
+        target: "_blank",
+        className: "theme-darkred-".concat(theme)
       }, "@earningsfly"), " with ", _react["default"].createElement("span", {
         style: {
           fontSize: 16,
           color: 'red'
         }
-      }, "\u2764\uFE0F"), ", datasource: Google Trends"));
+      }, "\u2764\uFE0F")));
     }
   }]);
 
